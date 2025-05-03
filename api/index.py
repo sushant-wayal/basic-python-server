48295ef0aca0c64594fc0fdaacf130a05109ce9b
sushant-wayal <sushantssr12345@gmail.com>
2025-05-03 15:13:31 +0530

    Update index.py to set home route to '/' and enhance catch-all route for dynamic responses

--- api/index.py
+++ api/index.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/api')
-def catch_all(path):
+@app.route('/')
+def home():
     return 'Hello from Flask on Vercel!'
+
+@app.route('/<path:path>')
+def catch_all(path):
+    return f'You hit: /{path}'
