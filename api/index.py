4b9971043e76146427aafa257d145a740323167a
sushant-wayal <sushantssr12345@gmail.com>
2025-05-03 15:11:50 +0530

    Refactor index.py to change home route to /api and simplify response

--- api/index.py
+++ api/index.py
@@ -2,10 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello from Flask on Vercel!'
-
-@app.route('/<path:path>')
+@app.route('/api')
 def catch_all(path):
-    return f'You hit: /{path}'
+    return 'Hello from Flask on Vercel!'
